9db965747c019a9d81c0dfc92533fc45c163951a
harshit nand <nandharshit@gmail.comgit config --global user.name harshit>
2024-11-21 16:53:55 +0530

    update  index .js file

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { Provider } from 'react-redux'; // <-- Import Provider
 // // import store from './pages/store'; // <-- Import your Redux store
-
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <React.StrictMode>
     
-    
+    <BrowserRouter>  {/* Wrap App with BrowserRouter */}
       <App />
-   
+    </BrowserRouter>
 
-  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
